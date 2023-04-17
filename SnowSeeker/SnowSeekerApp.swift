ac7de87ee0d8647d352658ef7141ba8c862bd581
derya <derya@boonandmoil.dev>
2023-04-17 10:08:42 +0100

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Derya Antonelli on 17/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
