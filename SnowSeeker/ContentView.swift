c2f21780331a60e084d54d2b25d2807931815a6e
derya <derya@boonandmoil.dev>
2023-04-20 09:11:39 +0100

    initial state for project

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -8,26 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var searchText = ""
-    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
     
     var body: some View {
-        NavigationView {
-            List(filteredNames, id: \.self) { name in
-                Text(name)
-            }
-                .searchable(text: $searchText, prompt: "Look for something")
-                .navigationTitle("Searching")
-        }
+        Text("Hello, world!")
     }
     
-    var filteredNames: [String] {
-        if searchText.isEmpty {
-            return allNames
-        } else {
-            return allNames.filter { $0.localizedCaseInsensitiveContains(searchText) }
-        }
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
